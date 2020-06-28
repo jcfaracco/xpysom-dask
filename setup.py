7459df4ebf331159d6c47c39e3051fd008ced293
Riccardo Mancini <r.mancini@santannapisa.it>
2020-06-28 17:49:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-description = 'Minimalistic implementation of the Self Organizing Maps (SOM)'
+description = 'Minimalistic implementation of batch Self Organizing Maps (SOM)'
 keywords = ['machine learning', 'neural networks', 'clustering', 'dimentionality reduction']
 
-setup(name='MiniSom',
-      version='2.2.4',
+setup(name='XPySom',
+      version='1.0.0',
       description=description,
-      author='Giuseppe Vettigli',
+      author='Riccardo Mancini',
       package_data={'': ['Readme.md']},
       include_package_data=True,
       license="CC BY 3.0",
-      py_modules=['minisom'],
-      requires=['numpy'],
-      url='https://github.com/JustGlowing/minisom',
-      download_url='https://github.com/JustGlowing/minisom/archive/master.zip',
+      py_modules=['xpysom'],
+      requires=['numpy', 'cupy'],
+      url='https://github.com/Manciukic/xpysom',
+      download_url='https://github.com/Manciukic/xpysom/archive/master.zip',
       keywords=keywords)
