48e1552fce6dbc424fb4e9ec576b9ae742eac976
Riccardo Mancini <r.mancini@santannapisa.it>
2021-05-02 01:18:02 +0200

    update to version 1.0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ description = 'Minimalistic implementation of batch Self Organizing Maps (SOM) f
 keywords = ['machine learning', 'neural networks', 'clustering', 'dimentionality reduction']
 
 setup(name='XPySom',
-      version='1.0.6',
+      version='1.0.7',
       description=description,
       long_description=long_description,
       long_description_content_type="text/markdown",
